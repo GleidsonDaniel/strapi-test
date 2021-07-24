d7d169a83d1ab423cb463e48379f64f237e9a5b6
Daniel <gleidson10daniel@hotmail.com>
2021-07-23 21:50:40 -0300

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c2f6b2aaee39e69048927886454c7ce'),
+    },
+  },
+});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '91b4855a-2669-4892-bc5e-37436f210d25'
-};
